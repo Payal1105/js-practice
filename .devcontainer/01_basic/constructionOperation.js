51303c555d3005dfcbaad1a160d41e890830fbcd
payal_2025_Spade <payal.k@spadetechnologies.in>
2025-05-19 10:33:28 +0000

    done

---
+++ .devcontainer/01_basic/constructionOperation.js
@@ -0,0 +1,25 @@
+let score = undefined
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//NaN is Not a Number//
+//"55" => 55 number can easily convert//
+//"55ab" => NaN//
+//true => 1 ; false => 0//
+
+let isLoggedIn = "payal"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 = true ; 0 = false ; "" = false//
+
+let someNumber = 20
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
