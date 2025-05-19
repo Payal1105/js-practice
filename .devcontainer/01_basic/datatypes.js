2a9452da3b9b7e217feec663c62ba5c860f7c2b7
payal_2025_Spade <payal.k@spadetechnologies.in>
2025-05-19 08:28:08 +0000

    done

---
+++ .devcontainer/01_basic/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all javascript code newer version of JS//
+"use strict"; // treat all javascript code newer version of JS//
+
+let name = "payal" //this is string datatype use "" mostly//
+let age = 20 // this is number datatype//
+let isLoggedin = true // this is boolean datatype [true/false]//
+let state = null // this is nuul datatype, is always a standalone value//
+let city; //when we go for terminal it will show "undefined" as it is undefined datatype//
+
+// "sybol" is datatype used to uniquness//
+
+console.log(typeof age);
---
+++ .devcontainer/01_basic/variable.js
@@ -0,0 +1,6 @@
+const accountId ="1105"
+let accountEmail = "p@gmail.com"
+var accountPassword = "1234"
+accountCity = "Nagpur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
