cbe96719f6d750fa15d5929cfb7fc5c17bb2ac5e
Payal <payal.k@spadetechnologies.in>
2025-05-21 18:36:18 +0530

    done

---
+++ javascript/string.js
@@ -0,0 +1,20 @@
+const name = "payal"
+const myBooksCount = 20
+
+//old way to write code - console.log(name + myBookscount + "Comics")
+//string pattern - (`` this is backtivks sign help in string interpolation.) 
+// below is showing modern way syntax which is know as string interpolation.
+
+
+console.log(`Hey, my name is ${name} and I have ${myBooksCount} books`);
+
+const bookName = new String ('Winngs of fire')
+console.log(bookName [0]);
+console.log(bookName.__proto__);
+
+console.log(bookName.length);
+console.log(bookName.toUpperCase());
+console.log(bookName.charAt(2))
+
+
+
