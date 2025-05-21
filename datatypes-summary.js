54c5f733cafc730deabf94e08d4126f5072c9713
Payal <payal.k@spadetechnologies.in>
2025-05-21 15:20:17 +0530

    first commit

---
+++ datatypes-summary.js
@@ -0,0 +1,35 @@
+// # primitive
+//7 types- String, Number, Boolearn, Null, Unidentified, Symbol, BigInt
+
+//# reference type (non- primitive)
+//Types - Arrays, Objects, Functions
+
+//Example
+
+const name = ("payal");
+const score = 100;
+const isLoggedIn = false;
+const temperatue = null;
+let accountCity;
+const id = Symbol('123')
+const anotherId = Symbol('123');
+
+//Types - Arrays, Objects, Functions
+
+//array : always in square bracket []
+
+const flowers = ["mogra", "lily", "rose"]
+
+//object : are in { } brackets
+
+let Myobject = { 
+    name: "payal",
+    age: 25,
+
+}
+
+//function - (){} both brackets are there in function
+
+const Myfunction = function(){
+    console.log("hello world")
+}
