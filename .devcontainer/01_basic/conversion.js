71f37391a38d5d8b68bc70f9479e1a9358b8eb2f
payal_2025_Spade <payal.k@spadetechnologies.in>
2025-05-21 06:50:47 +0000

    done

---
+++ .devcontainer/01_basic/conversion.js
@@ -0,0 +1,13 @@
+// console.log(2>1);
+// console.log(3>=2);
+// console.log(2==2);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined==0)
+//remember that this type of comparisions always will leads to confusion (null,unidentified)//
---
+++ .devcontainer/01_basic/datatypes-summary.js
@@ -0,0 +1,16 @@
+// # primitive
+//7 types- String, Number, Boolearn, Null, Unidentified, Symbol, BigInt
+
+//# reference type (non- primitive)
+//Types - Arrays, Objects, Functions
+
+//Example
+
+const name = ("payal");
+const score = 100;
+const isLoggedIn = false;
+const temperatue = null;
+let accountCity;
+const id = Symbol('123')
+const anotherId = Symbol('123');
+
