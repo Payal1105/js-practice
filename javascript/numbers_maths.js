50c3069b1ee5964cce99731e26fc6738387296e8
Payal <payal.k@spadetechnologies.in>
2025-05-26 17:43:23 +0530

    commit

--- javascript/numbers_maths.js
+++ javascript/numbers_maths.js
@@ -1,9 +1,26 @@
-const score = 112
-console.log(score);
+const score = 1120000
+//console.log(score);
 
 
-const balance = new Number (121)
-console.log(balance);
-console.log(balance.toPrecision(3))
+const balance = new Number (121.7)
+//console.log(balance);
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(1));
+//console.log(score.toLocaleString())
+
+//-------------------------MATHS----------------------------------
+//console.log(Math);
+//console.log(Math.abs(-5));
+//console.log(Math.round(7.4));
+//console.log(Math.floor(9.9));
+//console.log(Math.ceil(4.1));
+//console.log(Math.min(2,4,5,67,8));
+//console.log(Math.max(2,4,5,67,8));
+
+console.log(Math.random()*10+1);
+
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random()*(max-min+1))+ min);
 
 
