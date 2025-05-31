c905e30243f49f312c845805e6bf74fc2518118b
Payal <payal.k@spadetechnologies.in>
2025-05-31 18:31:39 +0530

    done

---
+++ 02_javascript_advance/functions.js
@@ -0,0 +1,55 @@
+// function myNewFunction() {
+//     console.log("P");
+//     console.log("A");
+//     console.log("Y");
+//     console.log("A");
+//     console.log("L")
+// }
+
+// myNewFunction()
+
+// function add2numbers(number1, number2) {
+//     console.log(number1 +  number2)
+// }
+
+// add2numbers(4,6)
+
+function add2numbers(number1, number2) {
+    let result = number1 + number2
+     return number1 + number2
+    return result
+   
+
+}
+const result = add2numbers(3,5)
+//console.log("result: ", result);
+
+// function logInUserMessage(username) {
+//     if (username === undefined){
+//         console.log("please enter username" )
+//     }
+//     return `${username} just logged in`
+    
+//console.log (logInUserMessage("payal"))
+//console.log (logInUserMessage())
+
+//-------------------------------------------------->>
+//(...num1) this three dots is known as spread and rest operator.
+
+function calculateCartPrice(...num1){
+return num1
+}
+
+//console.log(calculateCartPrice(1890,1250,765,300,500));
+
+const user = {
+    username: "payal@123",
+    price: 299
+   
+}
+
+function handleObject (anyobject){
+    console.log(`username is ${anyobject.username} and the price is ${anyobject.price}. `);
+    
+}
+handleObject(user)
--- 02_javascript_advance/objects.js
+++ 02_javascript_advance/objects.js
@@ -37,13 +37,79 @@ JsUser.greetingTwo = function(){
 //-------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 const amazonUser = new Object()
-console.log(amazonUser)
+//console.log(amazonUser)
 const amazonUser2 = {}
-console.log(amazonUser2)
+//console.log(amazonUser2)
 amazonUser.Id = "123xyz"
 amazonUser.name = "ammy"
 amazonUser.isLoggedIn = false
 
-console.log(amazonUser)
+//console.log(amazonUser)
+
+const regularUser = {
+    email: "megha@google.in",
+    fullName: {
+        username:{
+            firstname: "payal",
+            lastname : "Kamble"
+        }
+
+    }
+}
+
+ //console.log(regularUser.fullName?.username.firstname)
 
 
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj3 = {5:"e", 6:"f"}
+
+const obj4 = {obj1,obj2}
+//console.log(obj4);
+const obj5 = Object.assign({},obj1,obj2,obj3)
+//console.log(obj5);
+
+const Users = [
+    {
+        id : 1,
+        email: "p@google.com"
+
+    },
+    {
+        id : 2,
+        email: "a@google.com"
+
+    },
+    {
+        id : 3,
+        email: "m@google.com"
+
+    }
+
+]
+Users[1].email
+//console.log(amazonUser)
+
+//console.log(Object.keys(amazonUser));
+//console.log(Object.values(amazonUser));
+
+//console.log(Object.entries(amazonUser));
+
+//---------------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+//Object De-structure ---->
+
+const course = {
+    coursename: "Json",
+    coursefees:999,
+    courseInstructor: "xyz"
+}
+//course.courseInstructor
+const{courseInstructor} = course
+console.log(courseInstructor);
+//console.log(instructor)
+
+
+
+ 
