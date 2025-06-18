c39a1c0723cc3281c023d58124362d7504de1c5e
Payal <payal.k@spadetechnologies.in>
2025-06-18 14:05:24 +0530

    changes in project one

--- projects/one.js
+++ projects/one.js
@@ -2,15 +2,26 @@ document.getElementById("demo").textContent = "Hello from JS!";
 
 
 
-const buttons =document.querySelectorAll('.button')
-const body = document.querySelector("body")
-buttons.forEach(function(button){
-    console.log(button);
-    button.addEventListener('click', function(e){
-         console.log(e);
-          console.log(e.target);
-    })
-})
+// const buttons =document.querySelectorAll('.button')
+// const body = document.querySelector("body")
+// buttons.forEach(function(button){
+//     console.log(button);
+//     button.addEventListener('click', function(e){
+//          console.log(e);
+//           console.log(e.target);
+//     })
+// })
+
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        const color = e.target.id;
+        body.style.backgroundColor = color;
+        
+    });
+});
 
 if(e.target.id==='grey'){
     body.style.backgroundColor = e.target.id;
